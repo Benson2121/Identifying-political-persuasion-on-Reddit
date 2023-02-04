afff2fd666668916287c6252c208ccab3cc98727
Benson <benson.li@mail.utoronto.ca>
2023-02-04 15:47:26 -0500

    Update Commitment

--- Starter Files/preproc.py
+++ Starter Files/preproc.py
@@ -1,3 +1,10 @@
+#  * This code is provided solely for the personal and private use of students
+#  * taking the CSC401 course at the University of Toronto. Copying for purposes
+#  * other than this use is expressly prohibited. All forms of distribution of
+#  * this code, including but not limited to public repositories on GitHub,
+#  * GitLab, Bitbucket, or any other online platform, whether as given or with
+#  * any changes, are expressly prohibited.
+#  
 #  * All of the files in this directory and all subdirectories are:
 #  * Copyright (c) 2020 Frank Rudzicz
  
